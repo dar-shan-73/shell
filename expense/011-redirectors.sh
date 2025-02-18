fe8f57a0c3e5e9a78f3cf0129bd92d3cc3f61d6f
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-18 23:13:16 +0530

    learning functions

---
+++ expense/011-redirectors.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# standard o/p
+cat file > log.op  here the o/p will be overwritten
+
+cat file >> log.op here the o/p will be appended
+
+
+# standard error
+2>
+
+
+# standard o/p and error
+
+lsbkkk &> 
+lsbklkd &>>
+
+
+# standard input
+mysql < /tmp/setup.sql
