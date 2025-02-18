8f4253cb3a8a3755c364059da586e065a1bfd204
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-18 19:01:54 +0530

    beginning of shell

--- expense/004-dynamicVars.sh
+++ expense/004-dynamicVars.sh
@@ -3,3 +3,9 @@ DATE=$(date +%F)
 #majorly used for dates
 
 echo "todays date is $DATE"
+
+#arguments
+#when you are not sure of who runs, dynamically chosen
+#EXEC=$1 is arg1 you need to supply the values at command line, helps to override the values
+#DEPARMENT=$2 IS arg2, you can mention like "script executed by #EXEC and is from $DEPARTMENT "it will take arg1=$1 and arg2=$2 
+
---
+++ expense/005-specialVars.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# comes with pre-defined values
+
+echo "script name is $0"
