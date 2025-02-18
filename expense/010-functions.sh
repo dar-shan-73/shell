8c2391e66d62a355a75b8297004ea367323cfd69
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-18 22:41:43 +0530

    learning functions

--- expense/010-functions.sh
+++ expense/010-functions.sh
@@ -3,11 +3,13 @@
 stat(){
     echo "today's date is $(date +%F)"
     echo "load avg on the system $(uptime)"
+    echo "$(whoami)"
+    echo "disk utilization $(df -h)"
 
 }
 
 stat
 
-echo "I call call the function one more time"
+echo "I'm calling the function one more time"
 
 stat
