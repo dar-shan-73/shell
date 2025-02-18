c4c586925ffe3269f251e4ca9d6a724e1428e42c
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-18 18:06:00 +0530

    beginning of shell

---
+++ expense/002-colors.sh
@@ -0,0 +1,14 @@
+!/bin/bash 
+
+# Each and every color you see on terminal will have a color code and we need to use that code baesd on our need.
+# Colors       Foreground          Background
+# Red              31                  41
+# Green            32                  42
+# Yellow           33                  43
+# Blue             34                  44
+# Magenta          35                  45
+# Cyan             36                  46
+
+# echo -e "\e[COLORCODEm I am printing a color \e[0m"
+
+echo -e "\e[32m this is green color \e[0m"
