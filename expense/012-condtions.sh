16bffd0fa2ac3a61233cfcde4e52b6785a273139
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-18 23:58:16 +0530

    learning functions

--- expense/012-condtions.sh
+++ expense/012-condtions.sh
@@ -9,10 +9,32 @@ echo -e "demo on if else condition"
 
 ACTION=$1
 
-if [ "$ACTION" = "start" ] ; then 
+# if else
+
+# if [ "$ACTION" = "start" ] ; then 
+#     echo -e "\e[32m backend is starting \e[0m"
+#     exit 0
+
+# else echo -e "\e[31m start is a valid option \e[0m"
+#     exit 1    
+# fi
+
+# elseif(elif)
+
+if ["$ACTION" = "start"] ; then
     echo -e "\e[32m backend is starting \e[0m"
     exit 0
 
-else echo -e "\e[31m start is a valid option \e[0m"
-    exit 1    
-fi
+elif [ "$ACTION" = "stop"] ; then 
+    echo -e "\e[31m backend is stopping \e[0m"
+
+    exit 1
+
+elif [ "$ACTION" = "restart"] ; then
+    echo -e "\e[31m backend is restarting \e[0m"
+    exit 2
+
+else
+    echo -e "\e[33m valid optiong are start stop restart \e[0m"
+    exit 3
+fi        
