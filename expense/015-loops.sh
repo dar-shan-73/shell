3a30d97badd7cdd7ef1ed76174efa3c774742aa2
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-19 16:50:14 +0530

    learning functions

--- expense/015-loops.sh
+++ expense/015-loops.sh
@@ -4,6 +4,7 @@
 # for - when you are sure to run how many times to run
 # while- when you are not sure to run how many times to run
 
+#!/bin/bash
 for var in a b c d e f ; do 
      echo " value is  $var"
 done     
