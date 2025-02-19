341dd796839cf9b4ab4fe5898c4e203af9196668
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-19 16:41:27 +0530

    learning functions

--- expense/015-loops.sh
+++ expense/015-loops.sh
@@ -1,2 +1,6 @@
-# to run something for some number of times
+# to run something for some n number of times on n number of servers
+
+
+# for - when you are sure to run how many times to run
+# while- when you are not sure to run how many times to run
 
